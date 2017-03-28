6b71b2677b7a00b323bbd19c1c4d43fdab4063f4
= <vuminhdao@outlook.com>
2017-03-28 10:08:28 +0700

    edit

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var wkhtmltoimage = require('./libs/wkhtmlimage');
 var wkhtmltopdf = require('./libs/wkhtmltopdf');
 var mupdf = require('./libs/mupdf');
 
-module.exports = { html2pdf: wkhtmltopdf, html2image, wkhtmltoimage, pdf2image: mupdf }
+module.exports = { html2pdf: wkhtmltopdf, html2image: wkhtmltoimage, pdf2image: mupdf }
